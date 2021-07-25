352e2c5379859a9d97fc7a918b0bb9cd211d72b8
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-07-25 17:35:42 -0300

    Adicionado a busca por users na rota DashboardController@users.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
 class DashboardController extends Controller
 {
-    public function users()
+    /**
+     * @return Factory|View|Application
+     * @throws Exception
+     */
+    public function users(): Factory|View|Application
     {
-        return view('dashboard.users');
+        $this->authorize('user_index');
+
+        $users = cache()->remember('dashboard-users', now()->addHour(), function () {
+            return User::with('role:id,name')->get();
+        });
+
+        return view('dashboard.users', compact('users'));
     }
 }
