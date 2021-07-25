ae416b814c9da098e8cc6ace461218fd5bddf835
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-07-25 16:41:15 -0300

    Ajustados as rotas e modificados algumas informações para se adequa ao sistema.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 require __DIR__.'/auth.php';
+
+Route::middleware('auth')->name('dashboard.')->group(function () {
+    Route::get('/', [ DashboardController::class, 'users' ])->name('users');
+});
