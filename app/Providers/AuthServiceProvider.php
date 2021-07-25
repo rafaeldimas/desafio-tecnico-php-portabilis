41168777735d3352b2e0622114c4e121a8533f12
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-07-25 16:23:20 -0300

    Configurado os gates para todas as permissions.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Permission;
+use App\Models\User;
+use Exception;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +31,35 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        try {
+            $this->registerUserGates();
+        } catch (Exception $e) {}
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function registerUserGates()
+    {
+        if (!Schema::hasTable('permissions')) {
+            return;
+        }
+
+        /** @var Permission[]|Collection $permissions */
+        $permissions = cache()->remember('all_permissions', now()->addHour(), function () {
+            return Permission::all();
+        });
+
+        $permissions->map(function(Permission $permission) {
+            Gate::define($permission->name, function(User $user) use ($permission) {
+                return $user
+                    ->role()
+                    ->where('active', true)
+                    ->whereHas('permissions', function (Builder $builder) use ($permission) {
+                        $builder->where('id', $permission->id);
+                    })
+                    ->exists();
+            });
+        });
     }
 }
