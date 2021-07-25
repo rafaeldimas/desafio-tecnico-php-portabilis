02bb35f9123f36222f02924ac072b62378d36b48
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-07-25 16:03:22 -0300

    Definido as colunas da tabela roles no campo fillable do model.

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Permission
+ * @package App\Models
+ *
+ * @property string $label
+ * @property string $name
+ *
+ * @mixin Builder
+ */
 class Permission extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'label',
+        'name',
+    ];
 }
