a734a13d58798be444bf75f624d64616f21bb3b7
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-07-25 18:54:14 -0300

    Adaptado a view para efetuar a busca e ordenação.

--- resources/views/dashboard/users.blade.php
+++ resources/views/dashboard/users.blade.php
@@ -5,6 +5,88 @@
         </h2>
     </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="{{ route('dashboard.users') }}">
+                        @csrf
+
+                        <div class="sm:flex">
+                            <div class="w-full sm:w-1/4 mx-2">
+                                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                                <div class="mt-1 relative rounded-md shadow-sm">
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        id="name"
+                                        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ request('name') }}"
+                                    >
+                                </div>
+                            </div>
+
+                            <div class="w-full sm:w-1/4 mx-2">
+                                <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
+                                <div class="mt-1 relative rounded-md shadow-sm">
+                                    <input
+                                        type="text"
+                                        name="email"
+                                        id="email"
+                                        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ request('email') }}"
+                                    >
+                                </div>
+                            </div>
+
+                            <div class="w-full sm:w-1/4 mx-2">
+                                <label for="role" class="block text-sm font-medium text-gray-700">Role</label>
+                                <div class="mt-1 relative rounded-md shadow-sm">
+                                    <input
+                                        type="text"
+                                        name="role"
+                                        id="role"
+                                        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                                        value="{{ request('role') }}"
+                                    >
+                                </div>
+                            </div>
+
+                            <div class="w-full sm:w-1/4 mx-2">
+                                <label for="order" class="block text-sm font-medium text-gray-700">Order</label>
+                                <div class="mt-1 relative rounded-md shadow-sm">
+                                    <select
+                                        name="order"
+                                        id="order"
+                                        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+                                    >
+                                        <option value="" selected disabled>Select Field</option>
+                                        <option value="name-asc" @if(request('order') === 'name-asc') selected @endif>Name/Asc</option>
+                                        <option value="name-desc" @if(request('order') === 'name-desc') selected @endif>Name/Desc</option>
+                                        <option value="email-asc" @if(request('order') === 'email-asc') selected @endif>Email/Asc</option>
+                                        <option value="email-desc" @if(request('order') === 'email-desc') selected @endif>Email/Desc</option>
+                                        <option value="role-asc" @if(request('order') === 'role-asc') selected @endif>Role/Asc</option>
+                                        <option value="role-desc" @if(request('order') === 'role-desc') selected @endif>Role/Desc</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="mt-3 text-right">
+                            <a href="{{ route('dashboard.users') }}" class="inline-flex justify-center py-2 px-4 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:bg-gray-200">
+                                Reset
+                            </a>
+
+                            <button type="submit" class="ml-2 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Search
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="py-12 max-w-7xl mx-auto flex flex-col">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
