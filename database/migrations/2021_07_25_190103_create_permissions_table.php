f28f9a46b6c308b5c6acf04d8db89a0a2bed366b
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-07-25 16:02:19 -0300

    Definido as colunas da tabela permissions.

--- database/migrations/2021_07_25_190103_create_permissions_table.php
+++ database/migrations/2021_07_25_190103_create_permissions_table.php
@@ -15,6 +15,8 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->id();
+            $table->string('label');
+            $table->string('name');
             $table->timestamps();
         });
     }
