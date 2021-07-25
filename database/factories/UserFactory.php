b8ba985fcdfbf9ab643230e08c8de3aa051ed629
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-07-25 16:18:39 -0300

    Configurado a factory da entidade User.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -28,6 +29,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'active' => $this->faker->boolean,
+            'role_id' => Role::factory()->active(),
         ];
     }
 
@@ -41,6 +44,19 @@ class UserFactory extends Factory
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
+            ];
+        });
+    }
+
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin'),
+                'active' => true,
+                'role_id' => Role::first(),
             ];
         });
     }
