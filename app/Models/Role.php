9f0aeb3231f0f2041fad7760664b6ddead403a7c
Rafael Dimas <rafael.dimas@agilytecnologia.com.br>
2021-07-25 15:44:41 -0300

    Definido as colunas da tabela roles no campo fillable do model.

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Role
+ * @package App\Models
+ *
+ * @property string $name
+ * @property boolean $active
+ *
+ * @mixin Builder
+ */
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'active',
+    ];
+
+    protected $casts = [
+        'active' => 'bool',
+    ];
 }
